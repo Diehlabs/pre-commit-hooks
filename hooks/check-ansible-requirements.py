52484e3c063ab0a2dee62bdc2963424c21c2f480
Chris Diehl <cultclassik@gmail.com>
2022-02-14 09:46:19 -0600

    add setup.py

--- hooks/check-ansible-requirements.py
+++ hooks/check-ansible-requirements.py
@@ -1,15 +1,31 @@
+from __future__ import annotations
+
 import yaml
 from lastversion import has_update
 
-with open('../requirements.yml', 'r') as file:
-    ans_req = yaml.safe_load(file)
+def main():
+    with open('requirements.yml', 'r') as file:
+        ans_req = yaml.safe_load(file)
 
-for role in ans_req['roles']:
-    if 'name' in role:
-        role_name = role['name']
-        repo_url = role['src'].replace('.git', '')
-        role_version = role['version']
-        latest_version = has_update(repo=repo_url, current_version=role_version)
-        if latest_version:
-            print('Role {0} at {1} is version {2}'.format(role_name, repo_url, role_version))
-            print('Has new version: {0}'.format(latest_version))
+    retval = {}
+
+    for role in ans_req['roles']:
+        if 'name' in role:
+            role_name = role['name']
+            repo_url = role['src'].replace('.git', '')
+            role_version = role['version']
+            latest_version = has_update(repo=repo_url, current_version=role_version)
+            if latest_version:
+                retval[role_name] = {
+                    'current_version': role_version,
+                    'latest_version' : latest_version
+                }
+
+    return retval
+
+if __name__ == '__main__':
+    result = main()
+    if result:
+        print(result)
+    else:
+        print("All roles are using the latest version.")
